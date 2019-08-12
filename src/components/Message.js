95be353fef005bf5613e9808b6a756ae9c0a1dbb
Natalya Apasova <natalya.gulak.apasova@yandex.by>
2019-08-12 12:17:28 +0300

    refactor: change css styles, refactor message

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,16 +2,13 @@ import React from 'react';
 import './styles/layout.css';
 
 class Message extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
        <>
-          <p>From: {this.props.message.from}</p>
+        <div className="message">
+          <p>From: {this.props.message.from} ({this.props.message.date})</p>
           <p>{this.props.message.message}</p>
-          <p>Date: {this.props.message.date}</p>
+        </div>
        </>
     )
   }
